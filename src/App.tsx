a9fffd94d291ad1902c048746decc2f379892d35
shoislommm <shoislommm@bk.ru>
2026-08-04 16:54:59 +0500

    removed iframe and added handle resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import { AppProvider } from './store/app'
 
 export default function App() {
+   useEffect(() => {
+    const sendHeight = () => {
+      window.parent.postMessage(
+        {
+          type: "iframe-height",
+          height: document.documentElement.scrollHeight,
+        },
+        "*"
+      );
+    };
+
+    sendHeight();
+
+    const observer = new ResizeObserver(sendHeight);
+    observer.observe(document.body);
+
+    window.addEventListener("resize", sendHeight);
+
+    return () => {
+      observer.disconnect();
+      window.removeEventListener("resize", sendHeight);
+    };
+   }, []);
+  
   return (
     <AppProvider>
       <Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
-import '@iframe-resizer/child/index.umd.js';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
