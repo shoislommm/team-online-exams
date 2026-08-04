db342fae4275f72907b6265f2ee940d4047144e6
shoislommm <shoislommm@bk.ru>
2026-08-04 16:41:37 +0500

    fix iframe

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
-import '@iframe-resizer/child';
+import * as child from '@iframe-resizer/child';
+
+console.log(child);
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { inspectAttr } from 'plugin-inspect-react-code'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
