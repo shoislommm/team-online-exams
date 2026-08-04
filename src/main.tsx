4126196e2e33fe71190666a9ddbc20d82ce3b1eb
shoislommm <shoislommm@bk.ru>
2026-08-04 16:43:46 +0500

    testing iframe

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
-import * as child from '@iframe-resizer/child';
-
-console.log(child);
+import '@iframe-resizer/child/index.umd.js';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
