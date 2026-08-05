558bcc113fc5b58127e0dea668febcc992960f5f
shoislommm <shoislommm@bk.ru>
2026-08-05 16:12:54 +0500

    fixed syntax error

--- src/store/app.tsx
+++ src/store/app.tsx
@@ -375,18 +375,18 @@ export function AppProvider({ children }: { children: ReactNode }) {
     showPage("exam-interface");
     setExamRunning(true);
 
-  //   requestAnimationFrame(() => {
-  //     requestAnimationFrame(() => {
-  //       window.parent.postMessage(
-  //         {
-  //           type: "iframe-height",
-  //           height: document.documentElement.offsetHeight,
-  //         },
-  //         "*",
-  //       );
-  //     });
-  //   });
-  // }
+    // requestAnimationFrame(() => {
+    //   requestAnimationFrame(() => {
+    //     window.parent.postMessage(
+    //       {
+    //         type: "iframe-height",
+    //         height: document.documentElement.offsetHeight,
+    //       },
+    //       "*",
+    //     );
+    //   });
+    // });
+  }
 
   function startExam(type: string): void {
     startExamNow(type);
